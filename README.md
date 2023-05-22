6927012f0a9d11247082c730accbec10b2738a21
hedmana3 <axel.hedman@aalto.fi>
2023-05-23 00:01:29 +0300

    README.md update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # CHESS BOARD
+As a chess enthusiastic BioIT student I naturally have to have a home-made chess board to practice my openings on. I wanted to expand my knowledge in C++ and the SFML library so i took this as an opportunity to do exactly that. The result was a SFML based GUI application in the shape of a chess analysis board. Hit me up if you are looking for an opponent.
 
 ## Build instructions 
+Navigate to the project root directory and execute:
 ```
-sudo apt install build-essential
+./play.sh
 ```
---
+++ include/State.hpp
@@ -0,0 +1,28 @@
+#ifndef __STATE_H__
+#define __STATE_H__
+
+#include <SFML/System/Time.hpp>
+
+namespace Engine
+{
+    // PARENT CLASS FOR IMPLEMENTING GAME STATES
+    class State
+    {
+        public:
+            // Constructor and destructor:
+            State(){};
+            virtual ~State(){};
+
+            // Pure virtual methods:
+            virtual void Init() = 0;
+            virtual void PollEvent() = 0;
+            virtual void Update(sf::Time deltaTime) = 0;
+            virtual void Draw() = 0; 
+
+            // Virtual methods:
+            virtual void Pause(){};
+            virtual void Start(){};
+    };
+}
+
+#endif // __STATE_H__
