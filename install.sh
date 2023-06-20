44a1a0336423d2de052cce03a74b0e599d7c37f8
Axel Hedman <h3dmanaxel@gmail.com>
2023-06-20 12:57:55 +0300

    Cmake update

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-REQUIRED_PKG="build-essential cmake"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  sudo apt-get --yes install $REQUIRED_PKGS
-fi
-
 DIR="${PWD}/build"
 if [ -d "$DIR" ]; then
   echo "Rebuilding..."
